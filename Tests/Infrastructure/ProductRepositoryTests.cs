629c504aabc6f0770b6866d053cb9042a264386a
MohadmadReza <mr.rahmatzade371@gmail.com>
2024-05-10 12:17:52 -0600

    add test for persistence layer

---
+++ Tests/Infrastructure/ProductRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Infrastructure.Persistence.Contexts;
+using Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests.Infrastructure
+{
+    public class ProductRepositoryTests
+    {
+        [Fact]
+        public async Task AddAsync_ProductIsAdded()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "ProductDb")
+                .Options;
+
+            using (var dbContext = new AppDbContext(options))
+            {
+                var repository = new ProductRepository(dbContext);
+                var product = new Product { Name = "New Product", Price = 150m };
+
+                // Act
+                await repository.CreateAsync(product);
+
+                // Assert
+                var fetchedProduct = await dbContext.Products.FindAsync(product.Id);
+                Assert.NotNull(fetchedProduct);
+                Assert.Equal("New Product", fetchedProduct.Name);
+                Assert.Equal(150m, fetchedProduct.Price);
+            }
+        }
+    }
+}
